fe417a5e26d33e677d10a8d449f0d9a5932d410e
beam257 <146344430+beam257@users.noreply.github.com>
2023-09-28 19:02:31 +0700

    Add files via upload

---
+++ login.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Website With Login & Registration | Form codehal</title>
+    <link rel="stylesheet" href="style1.css">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+</head>
+
+<body>
+	
+    
+
+    <div class="container">
+        <div class="content">
+            <a href="index1.php" style="text-decoration: none;"><font color="#003300"><h2 class="Logo"><i class='bx bxl-firebase'></i>Bookshop</h2></a></font>
+            <div class="text-sci">
+                <h2>Welcome!<br><span>To Website Bookshop.</span></h2>
+
+                <p>The bookstore has many books. Free shipping nationwide</p>
+
+                <div class="social-icon">
+                    <a href="#"><i class='bx bxl-linkedin'></i></a>
+                    <a href="#"><i class='bx bxl-facebook'></i></a>
+                    <a href="#"><i class='bx bxl-instagram'></i></a>
+                    <a href="#"><i class='bx bxl-twitter'></i></a>
+                </div>
+
+            </div>
+        </div>
+            
+            <div class="logreg-box">
+            <div class="form-box Login">
+                <form action="backhome.php">
+                    <h2>Sign In</h2>
+
+                    <div class="input-box">
+                        <span class="icon"><i class='bx bxs-envelope'></i></span>
+                        <input type="email" required>
+                        <label>email</label>
+                    </div>
+
+                    <div class="input-box">
+                        <span class="icon"><i class='bx bxs-lock-alt'></i></span>
+                        <input type="password" required>
+                        <label>password</label>
+                    </div>
+
+                    
+                    <button type="submit" class="btn">Sign In</button>
+                    <a href="backhome.php"></a>
+                    
+                </form>
+            </div>
+        </div>
+    </div>
+
+    <script src="script.js"></script>
+</body>
+
+</html>
